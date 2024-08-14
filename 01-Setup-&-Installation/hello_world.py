593f0e8f5f6893c72a3b37aa9607e318c993b0f2
Damien Sawyer <damiensawyer@users.noreply.github.com>
2024-08-14 06:41:28 +0000

    this is a nice commit

--- 01-Setup-&-Installation/hello_world.py
+++ 01-Setup-&-Installation/hello_world.py
@@ -1,8 +1,9 @@
 print("hello, world!")
+print("hello, world again!")
 print("hello, world!")
-print("hello, world!")
-print("yay world!")
+print("yay world - isn't coding great!")
 
 # This is a comment... it will not be run.
 
+# This is a new comment
 
